0721581ce3653e2f469920ca1f3d186bf9142eb1
Huu Nguyen <huu@prismskylabs.com>
2013-10-31 20:55:15 -0700

    Allow post to questions

--- api/views.py
+++ api/views.py
@@ -11,3 +11,10 @@ class QuestionList(APIView):
         questions = Question.objects.all()
         serializer = QuestionSerializer(questions, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = QuestionSerializer(data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
