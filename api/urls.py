95b0d879b31f47978284c206f30ab2838184f419
Huu Nguyen <huu@prismskylabs.com>
2013-11-03 00:20:36 -0700

    One final fix

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
 urlpatterns = patterns('',
-    url(r'^quizzes/?', views.QuizList.as_view()),
+    url(r'^quizzes/$', views.QuizList.as_view()),
     url(r'^quizzes/fractions/$', views.FractionQuiz.as_view()),
     url(r'^questions/$', views.QuestionList.as_view()),
     url(r'^questions/(?P<pk>[0-9]+)/$', views.QuestionDetail.as_view()),
